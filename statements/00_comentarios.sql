6f777b0b478c3572acba16dea84214ddb5596b4f
imannolM <imonroydj@gmail.com>
2025-02-25 23:07:49 -0600

    Actualizamos la capeta statements

---
+++ statements/00_comentarios.sql
@@ -0,0 +1,15 @@
+-- Este es un comentario
+
+/* 
+SELECT * FROM hello_mysql.users ORDER BY age DESC;
+
+SELECT * FROM hello_mysql.users WHERE email="imonroydj@gmail.com" ORDER BY age DESC;
+
+SELECT name FROM hello_mysql.users WHERE email="imonroydj@gmail.com" ORDER BY age DESC;
+*/
+
+SELECT * FROM hello_mysql.users ORDER BY age DESC;
+
+SELECT * FROM hello_mysql.users WHERE email="imonroydj@gmail.com" ORDER BY age DESC;
+
+SELECT name FROM hello_mysql.users WHERE email="imonroydj@gmail.com" ORDER BY age DESC;
---
+++ statements/08_null.sql
@@ -0,0 +1,8 @@
+SELECT * FROM hello_mysql.users;
+
+SELECT * FROM hello_mysql.users WHERE email IS NULL;
+
+SELECT * FROM hello_mysql.users WHERE email IS NOT NULL;
+
+SELECT * FROM hello_mysql.users WHERE email IS NOT NULL AND age=15;
+
